9e15c45a40350600c54266d27495a52d74e09b73
DeepeshShukla <deepeshshukla471@gmail.com>
2025-11-21 02:48:43 +0530

    Update redux functionality

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -4,7 +4,7 @@ const userSlice=createSlice({
     name:"user",
     initialState:{
         userData:null
-    },//setUserData("ankush")<={payload}
+    },//setUserData("deepesh")<={payload}
     reducers:{
         setUserData:(state,action)=>{
         state.userData=action.payload
@@ -14,3 +14,15 @@ const userSlice=createSlice({
 
 export const {setUserData}=userSlice.actions
 export default userSlice.reducer
+
+// ✔ setUserData is a reducer (a function that updates state)
+// ✔ action.payload -> This is the data you send when dispatching.
+// ✔ userSlice.reducer -> Goes into the store.
+// ✔ userSlice.actions -> Used by components to update state.
+
+// How to update Redux state (useDispatch)
+// dispatch(setUserData("Deepesh"))
+// state.user.userData = "Deepesh"
+
+// How to read Redux state (useSelector)
+// const user = useSelector(state => state.user.userData);
